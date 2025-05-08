56ecd7c7170fb670a4f0c2a1ab85119dfcb7e9a8
asmitty92 <asmitty92@users.noreply.github.com>
2025-05-08 19:26:28 +0000

    Auto-format changes and new build output

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-import { Card, CardHand } from "aces-high-core";
+import { Card, CardHand, Face } from "aces-high-core";
 export declare enum PokerHands {
     HIGH_CARD = 1,
     ONE_PAIR = 2,
@@ -11,23 +11,24 @@ export declare enum PokerHands {
     STRAIGHT_FLUSH = 9
 }
 export declare const faceValues: Record<string, number>;
-export declare class PokerHand extends CardHand {
+export type PokerCard = Card<Face>;
+export declare class PokerHand extends CardHand<Face> {
     private myKicker;
     private _fullHouseTop;
     private _fullHouseBottom;
-    get kicker(): Card;
+    get kicker(): PokerCard;
     get fullHouseTop(): number;
     get fullHouseBottom(): number;
-    constructor(cards: Card[]);
+    constructor(cards: PokerCard[], accessKey: symbol);
     calculateScore(): number;
-    protected scoreSevenCardHand(cards: Card[]): number;
-    protected scoreFiveCardHand(cards: Card[]): number;
-    protected scoreNoSetsHand(cards: Card[]): PokerHands;
-    protected findKicker(cards: Card[], score: PokerHands): Card;
-    protected buildValuesMap(cards: Card[]): Record<string, number>;
+    protected scoreSevenCardHand(cards: PokerCard[]): number;
+    protected scoreFiveCardHand(cards: PokerCard[]): number;
+    protected scoreNoSetsHand(cards: PokerCard[]): PokerHands;
+    protected findKicker(cards: PokerCard[], score: PokerHands): PokerCard;
+    protected buildValuesMap(cards: PokerCard[]): Record<string, number>;
     protected findNonPairFaces(valuesMap: Record<string, number>): string[];
-    protected isStraight(cards: Card[]): boolean;
-    protected isFlush(cards: Card[]): boolean;
+    protected isStraight(cards: PokerCard[]): boolean;
+    protected isFlush(cards: PokerCard[]): boolean;
     protected findFullHouseTopAndBottom(valuesMap: Record<string, number>): number[];
     protected containsThree(valuesMap: Record<string, number>): boolean;
 }
